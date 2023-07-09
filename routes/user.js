8ca12b420bb6dd9259aed5a5df1040a6ff496e62
fancymithun <callmeprabhu7@gmail.com>
2023-07-09 15:59:33 +0530

    updated routes

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const userController = require("../controllers/userController")
 
 router.post("/signup", userController.signup)
 router.post("/signin", userController.signin)
-router.delete("/user",userController.deleteUser)
+router.delete("/user/:id",userController.deleteUser)
 router.get("/user/:id",userController.getUser)
 router.get("/users",userController.getAllUsers)
 
